ea454d7705bf1cf5060dc063d4f6e055855bac9f
Chiranth A M <chiranth.ajjamanemanohar@ucdenver.edu>
2026-04-05 18:44:41 -0600

    completed pattern till 17

--- Basics/build up logical thinking.cpp
+++ Basics/build up logical thinking.cpp
@@ -340,7 +340,7 @@ void pattern11( int n)
     //          [i, 1, 0 ]
     //1         [1, 1,  ]
     //1 1       [2, 2,  ]
-    //1 1 1 
+    //1 1 1     [3, 3,  ]
 
     for( int i=1; i<=n; i++)
     {
@@ -348,25 +348,242 @@ void pattern11( int n)
         //print 1
         //if i is even it starts from 0
         //if i is odd it starts from 1
+        int start =  ( i%2 == 0) ? start = 0 : start = 1 ;
 
-
-
+        // j runs i times.
         for ( int j=1; j<=i ;j++)
         {
-            int temp = i+j;
-            if( temp%2 == 0)
-            {
-                cout << "1 ";
-            }
-            else
-            {
-                cout << "0 ";
-            }
+            cout << start << " ";
+            start = 1-start;
         }
         cout << endl;
     }
 }
- 
+
+void pattern12(int n)
+{
+    // 1             1
+    // 1 2         2 1
+    // 1 2 3     3 2 1
+    // 1 2 3 4 4 3 2 1
+
+    // outer loop runs as many rows are present
+    for ( int i=1; i<=n; i++)
+    {
+        // each row prints number, space , number
+        
+        // i=3, row, i=3 ,numbers printed will be three
+        for ( int j=1; j<=i ; j++)
+        {
+            cout << j << " ";
+        }
+        
+        // i =1. space = 4  2(n-i)
+        // i =2, space = 2
+        // i =3, space = 0  2(n-i)
+        for ( int j=1; j<= 2*(n-i)  ; j++)
+        { 
+            cout << "  " ;
+        }
+
+
+        for ( int j=1; j<=i ; j++)
+        {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+}
+
+void pattern13( int n)
+{
+    // 1 
+    // 2 3 
+    // 4 5 6
+    // 7 8 9 10
+
+    // outer loop runs  as many times as number of rows are present.
+    for ( int i=1; i<=n; i++ )
+    {
+        // i =1, number printeed = 1 (1)
+        // i =2 , number printed = 2 3 (2)
+        // i =3 , number printed = 4 5 6 (3)
+
+        //inner loop prints numbers
+        int start = 1;
+        for( int j=1; j<=i; j++)
+        {
+            cout << start << " ";
+            start++;
+        }
+
+        //end the printing of j line
+        cout << endl;
+
+    }
+}
+
+void pattern14( int n)
+{
+    // A 
+    // A B  
+    // A B C
+    // A B C D
+
+    // outer loop runs n times
+    for ( int i=1; i<=n; i++)
+    {
+        // i=1 , A(1)
+        // i=2 , A B(2) 
+        // i=3 , A B C(3) = i
+        
+        string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int start=0;
+        for ( int j=1; j<=i ; j++)
+        {
+            cout << s[start] << " ";
+            start++;
+        }
+
+        // end of the line
+        cout << endl;
+    }
+
+}
+
+void pattern15( int n)
+{
+    // A B C 
+    // A B  
+    // A 
+
+    //outer loop runs n times
+    for ( int i=1; i<=n; i++)
+    {
+        // n=3
+        // i=1, 3, A B C
+        //   2, 2, A B
+        //   3, 1, A
+
+        string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int start=0;
+        
+        // loop runs till n-i+1
+        for ( int j=1; j<=n-i+1; j++)
+        {
+            cout << s[start] << " ";
+            start++;
+        }
+
+        // end of each line
+        cout << endl;
+
+    }
+    
+}
+
+void pattern16( int n)
+{
+    // A 
+    // B B  
+    // C C C
+
+    int start=0;
+    string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // outer loop runs n times
+    for ( int i=1; i<=n; i++)
+    {
+        // i=1, 1, A
+        // i=2, 2, B B
+        // i=3, 3, C C C 
+
+
+        //inner loop runs i time
+        for ( int j=1; j<=i; j++)
+        {
+            cout << s[start] << " ";
+        }
+        // move to next charachter
+        start++;
+
+        // End of line
+        cout << endl;
+    }
+    
+}
+
+void pattern17( int n)
+{
+    //     A 
+    //   A B A 
+    // A B C B A
+
+    //string
+    int start=0;
+    string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // outer loop runs n times
+    for ( int i=1; i<=n; i++)
+    {
+        //n=3
+        // i, space, charachter , space
+        // 1, 2, 1, 2
+        // 2, 1, 3, 1
+        // 3, 0, 5, 0
+         
+        //space n-i
+        for ( int j=1; j<= n-i; j++)
+        {
+            cout << "  ";
+        }
+
+        start = 0;
+        //charachter 2i-1
+        for ( int j=1; j<=2*i-1; j++)
+        {
+            cout << s[start] << " ";
+
+            // A B A
+            // i=1, A.      1
+            // i=2, A B A , 1,2,1
+            // i=3,         1,2,3,2,1
+            //need to flip
+            if( j<= i-1)
+            {
+                start++;
+            }
+            else
+            {
+                start--;
+            }
+            
+        }
+
+        //space n-i
+        for ( int j=1; j<= n-i; j++)
+        {
+            cout << "  ";
+        }
+
+        //end of line
+        cout << endl;
+
+    }
+
+}
+
+void pattern18( int n)
+{
+    // C 
+    // B C 
+    // A B C 
+
+}
+
+
 int main()
 {
     pattern1(3);
@@ -404,5 +621,25 @@ int main()
 
     pattern11(3);
     cout << endl;
+
+    pattern12(3);
+    cout << endl;
+
+    pattern13(3);
+    cout << endl;
+
+    pattern14(3);
+    cout << endl;
+
+    pattern15(3);
+    cout << endl;
+
+    pattern16(3);
+    cout << endl;
+
+
+    pattern17(3);
+    cout << endl;
+
 
 }
